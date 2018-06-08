fc790f9baac7768d7a11739f4180d48fca97ed6b
jeremy hernandez <jermhern@gmail.com>
2018-06-08 11:22:16 -0700

    added spawn position for player

--- js/app.js
+++ js/app.js
@@ -14,10 +14,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = (this.x + 10) * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    this.x = 1;
+    this.y = 500;
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -25,28 +28,30 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-    constructor(spriteImg) {
+    constructor() {
         this.sprite = 'images/char-boy.png';
+        this.x = 200;
+        this.y = 390;
     }
 
     update() {
-
+        
     }
 
     render() {
-
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
-    handleInput() {
-        
+    handleInput(allowedKeys) {
+
     }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const allEnemies = [];
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
